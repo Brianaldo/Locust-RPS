e40b1a973eefd6ef68f083d627386c514d17bdb2
Brianaldo <brianaldophandiarta31@gmail.com>
2024-05-07 14:30:54 +0700

    feat: load test with shape

--- locustfile.py
+++ locustfile.py
@@ -1,16 +1,5 @@
-from locust import HttpUser, constant_pacing, task, events
+from locust import HttpUser, LoadTestShape, between, task, events
 from requests.adapters import HTTPAdapter
-from locust.exception import StopUser
-import pandas as pd
-import gevent
-
-# Load the CSV
-df = pd.read_csv('freq.csv', parse_dates=[0])
-df.sort_values('time', inplace=True)
-total_row = len(df.index)
-start_time = df['time'].iloc[0]
-
-counter = 0
 
 class WebsiteUser(HttpUser):
     def __init__(self, *args, **kwargs):
@@ -18,26 +7,40 @@ class WebsiteUser(HttpUser):
         self.client.mount('https://', HTTPAdapter(pool_maxsize=300))
         self.client.mount('http://', HTTPAdapter(pool_maxsize=300))
 
-    wait_time = constant_pacing(1)
-
-    def scheduled_task(self):
-        self.client.get("/s0", name="HTTP Request")
+    wait_time = between(1, 5)
 
     @task
     def load_test(self):
-        global counter
-
-        if counter > total_row:
-            raise StopUser()
-
-        time_diff = int((df['time'].iloc[counter] - start_time).total_seconds())
-        if time_diff == counter:
-            frequency = df['frequency'].iloc[counter]
-            task_queue = [gevent.spawn(self.scheduled_task) for _ in range(frequency)]
-            gevent.joinall(task_queue)
-            
-        counter += 1
+        self.client.get("/s0", name="HTTP Request")
         
+class StagesShapeWithCustomUsers(LoadTestShape):
+
+    stages = [
+        {"duration":  5 * 60, "users": 5, "spawn_rate": 1, "user_classes": [WebsiteUser]},
+        {"duration":  5 * 60, "users": 10, "spawn_rate": 1, "user_classes": [WebsiteUser]},
+        {"duration": 10 * 60, "users": 30, "spawn_rate": 2, "user_classes": [WebsiteUser]},
+        {"duration": 10 * 60, "users": 50, "spawn_rate": 2, "user_classes": [WebsiteUser]},
+        {"duration": 10 * 60, "users": 100, "spawn_rate": 2, "user_classes": [WebsiteUser]},
+        {"duration":  5 * 60, "users": 300, "spawn_rate": 10, "user_classes": [WebsiteUser]},
+        {"duration":  5 * 60, "users": 200, "spawn_rate": 5, "user_classes": [WebsiteUser]},
+        {"duration":  10 * 60, "users": 10, "spawn_rate": 2, "user_classes": [WebsiteUser]},
+    ]
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                try:
+                    tick_data = (stage["users"], stage["spawn_rate"], stage["user_classes"])
+                except:
+                    tick_data = (stage["users"], stage["spawn_rate"])
+                return tick_data
+
+        return None
+    
 @events.test_stop.add_listener
 def on_test_stop(environment, **kwargs):
     print("Load test completed.")
+
+# locust -f locustfile.py --headless -H http://34.87.71.66:31113 --run-time 60m --stop-timeout 30s
