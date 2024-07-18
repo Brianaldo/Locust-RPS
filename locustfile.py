8fc3aef600e73cc67dac693eee5e67b9f96a862d
Brianaldo <brianaldophandiarta31@gmail.com>
2024-07-18 13:35:15 +0700

    feat: update test config

--- locustfile.py
+++ locustfile.py
@@ -7,10 +7,9 @@ import gevent
 # Load the CSV
 df = pd.read_csv('freq.csv', parse_dates=[0])
 df.sort_values('time', inplace=True)
-df = df.tail(24 * 60 * 60)
-total_row = len(df.index)
-start_time = df['time'].iloc[0]
-
+test_df = pd.DataFrame(df[344986:366586])
+total_row = len(test_df)
+start_time = test_df['time'].iloc[0]
 counter = 0
 
 
@@ -32,10 +31,10 @@ class WebsiteUser(HttpUser):
         if counter > total_row:
             raise StopUser()
 
-        time_diff = int((df['time'].iloc[counter] -
+        time_diff = int((test_df['time'].iloc[counter] -
                         start_time).total_seconds())
         if time_diff == counter:
-            frequency = df['frequency'].iloc[counter]
+            frequency = test_df['frequency'].iloc[counter]
             task_queue = [gevent.spawn(self.scheduled_task)
                           for _ in range(frequency)]
             gevent.joinall(task_queue)
